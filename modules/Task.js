9755726321d4a8791d4d55c10b108b3fa10f44e3
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-26 18:51:09 +0000

    Add utility function to create List

--- modules/Task.js
+++ modules/Task.js
@@ -1,8 +1,9 @@
-import { li } from './List.js';
+import { input, icon } from './List.js';
 
 class Utility {
   constructor() {
     this.temp = 0;
+    this.ul = 0;
   }
 
   add(data) {
@@ -32,13 +33,55 @@ class Utility {
     for (let i = 0; i < TODOS.length; i += 1) {
       const task = TODOS[i];
       const { description } = task;
-      li(description);
-      ul.append(li(description));
+
+      const li = this.createList(description, i);
+      ul.appendChild(li);
     }
   };
 
-  // delete() {
-  // };
+makeTextEditable = (div2, li) => {
+  div2.contentEditable = true;
+  li.style.backgroundColor = 'rgb(243, 243, 243)';
+  div2.focus();
+
+  const icon = document.querySelector('.icon');
+  icon.className = 'fa fa-heart';
+};
+
+  createList = (description, i) => {
+    const li = document.createElement('li');
+    const div = document.createElement('div');
+    const div2 = document.createElement('div');
+
+    li.draggable = true;
+    div2.className = 'editable';
+    div.className = 'checkbox-description-container';
+
+    div2.innerHTML = description;
+    div.append(input('checkbox'), div2);
+    li.append(div, icon('fas fa-ellipsis-v'));
+
+    li.className = 'list';
+
+    li.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.makeTextEditable(div2, li);
+    });
+
+    return li;
+  }
+
+  removeChild() {
+    this.ul = document.querySelector('.list-container');
+    while (this.ul.firstChild) {
+      this.ul.removeChild(this.ul.firstChild);
+    }
+  }
+
+  delete() {
+    this.removeChild();
+    localStorage.clear();
+  }
 
   // update() {
 

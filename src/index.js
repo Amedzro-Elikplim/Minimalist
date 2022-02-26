654d8c8830261e7f4668a3a48768e88b810d08b4
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-26 12:06:38 +0000

    Add a function to display todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './style.css';
+import { li, input, icon } from '../modules/List.js';
 
-console.log('minimalist you sup my gee');
+const TODOS = [
+  {
+    description: 'Washing the dishes',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete To Do list project',
+    completed: false,
+    index: 1,
+  },
+];
+
+const showTasks = () => {
+  const ul = document.querySelector('.list-container');
+
+  for (let i = 0; i < TODOS.length; i += 1) {
+    const task = TODOS[i];
+    const { description } = task;
+    li().append(input('checkbox'), icon());
+    ul.appendChild(li(description));
+  }
+};
+
+showTasks();

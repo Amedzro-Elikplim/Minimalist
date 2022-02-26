4d6db6c9fb54015d297e086840802b8d3e8c7d52
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-02-26 18:50:31 +0000

    Refactor code to respond to drag event

--- modules/List.js
+++ modules/List.js
@@ -5,27 +5,14 @@ const input = (type) => {
   return input;
 };
 
-const icon = () => {
+const icon = (name) => {
   const i = document.createElement('i');
-  i.className = 'fas fa-ellipsis-v icon';
+  i.className = `${name} icon`;
 
   return i;
 };
 
-const li = (description) => {
-  const li = document.createElement('li');
-  const div = document.createElement('div');
-
-  div.append(input('checkbox'), description);
-  li.append(div, icon());
-
-  li.className = 'list';
-  div.className = 'checkbox-description-container';
-  return li;
-};
-
 export {
-  li,
   input,
   icon,
 };

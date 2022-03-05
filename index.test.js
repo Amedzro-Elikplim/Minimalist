e2e6a92d41995d5beb23e7dbc52af85edcd13eff
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-05 10:40:11 +0000

    Add test for updating completed status, description and removing completed tasks

--- index.test.js
+++ index.test.js
@@ -1,27 +1,30 @@
 const Task = require('./modules/Task.js');
-const { saveToLocalStorage } = require('./modules/Storage.js');
+const { saveToLocalStorage, getFromLocalStorage } = require('./modules/Storage.js');
+
+const task = new Task();
+const array = [
+  {
+    description: 'Go to work',
+    index: 1,
+    completed: false,
+  },
+  {
+    description: 'Go to School',
+    index: 2,
+    completed: true,
+  },
+  {
+    description: 'Go to home',
+    index: 3,
+    completed: false,
+  },
+];
+
+beforeEach(() => {
+  saveToLocalStorage('tasks', array);
+});
 
 describe('updateIndex', () => {
-  const array = [
-    {
-      description: 'Go to work',
-      index: 1,
-      completed: false,
-    },
-    {
-      description: 'Go to School',
-      index: 2,
-      completed: false,
-    },
-    {
-      description: 'Go to home',
-      index: 3,
-      completed: false,
-    },
-  ];
-
-  const testTask = new Task();
-
   test('Test updateIndex', () => {
     const result1 = {
       description: 'Go to home',
@@ -35,78 +38,71 @@ describe('updateIndex', () => {
       completed: false,
     };
 
-    expect(testTask.updateIndex(0, array)).toContainEqual(result1);
-    expect(testTask.updateIndex(1, array)).toContainEqual(result2);
+    expect(task.updateIndex(0, array)).toContainEqual(result1);
+    expect(task.updateIndex(1, array)).toContainEqual(result2);
   });
 });
 
-describe('add', () => {
-  const mockLocalStorage = (() => {
-    let store = {};
-
-    return {
-      getItem(key) {
-        return store[key] || null;
-      },
-      setItem(key, value) {
-        store[key] = value.toString();
-      },
-      clear() {
-        store = {};
-      },
-    };
-  })();
-
-  const array = [
-    {
-      description: 'Go to work',
-      index: 1,
-      completed: false,
-    },
-    {
-      description: 'Go to School',
-      index: 2,
-      completed: false,
-    },
-    {
-      description: 'Go to home',
-      index: 3,
-      completed: false,
-    },
-  ];
-
-  beforeAll(() => {
-    Object.defineProperty(window, 'localStorage', {
-      value: mockLocalStorage,
-    });
-  });
-
+describe('add list to task in localStorage', () => {
   test('save to localStorage', () => {
     saveToLocalStorage('tasks', array);
     expect(JSON.parse(window.localStorage.getItem('tasks'))).toEqual(array);
   });
 });
 
-describe('cleare all checked', () => {
-  test('', () => {
+describe('delete task', () => {
+  const ul = document.createElement('ul');
+  ul.className = 'list-container';
+  ul.innerHTML = `
+    <li> Go to work </li>
+    <li> Buy groceries </li>
+  `;
+
+  test('remove all task from list', () => {
+    document.body.appendChild(ul);
+    expect(task.removeChild()).toBe(undefined);
+  });
+
+  const result = {
+    description: 'Go to work',
+    index: 1,
+    completed: false,
+  };
+
+  test('remove completed task from list', () => {
     const testTask = new Task();
-    const testArray = [
-      {
-        description: 'Go to work',
-        index: 1,
-        completed: false,
-      },
-      {
-        description: 'Go to School',
-        index: 2,
-        completed: true,
-      },
-      {
-        description: 'Go to home',
-        index: 3,
-        completed: true,
-      },
-    ];
-    expect(testTask.removeCompleted()).toContain(testArray.filter((e) => e.completed === false));
-  })
-})
+    expect(testTask.removeCompleted()).toContainEqual(result);
+  });
+});
+
+describe('update', () => {
+  test('update task description of second list', () => {
+    const updated = task.update('Take a break', 1);
+    const tasks = getFromLocalStorage('tasks');
+
+    expect(tasks[1]).toEqual(updated);
+  });
+
+  test('update task description of first list', () => {
+    const updated = task.update('Walk the dog', 0);
+    const tasks = getFromLocalStorage('tasks');
+
+    expect(tasks[0]).toEqual(updated);
+  });
+});
+
+describe('update completed status', () => {
+  test('check completed status of first list to true', () => {
+    const status = task.checked(true, 1);
+    const tasks = getFromLocalStorage('tasks');
+
+    expect(status).toEqual(tasks[1].completed);
+  });
+
+  test('check completed status of second list to false', () => {
+    const status = task.checked(false, 1);
+    const tasks = getFromLocalStorage('tasks');
+
+    expect(status).toEqual(tasks[1].completed);
+  });
+});
